fd900736d6406bfc5d557854576c9a292a7d4d97
CadentheCannon <118554615+CadentheCannon@users.noreply.github.com>
2024-04-17 14:31:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
 
